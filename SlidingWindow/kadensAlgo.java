ddc945f602512044cc2ed4a87f82e1ceabc179b5
Pranav Paliwal <pranavpaliwal6@gmail.com>
2024-09-23 21:54:23 +0530

    Added practice codes

---
+++ SlidingWindow/kadensAlgo.java
@@ -0,0 +1,26 @@
+public class kadensAlgo {
+
+        public static void main(String[] args){
+    
+            int arr[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+            int start=0;
+            int end=0;
+
+            int currsum=0;
+            int max=Integer.MIN_VALUE;
+            for(int i=0;i<arr.length;i++){
+                currsum+=arr[i];
+                
+                if(max<currsum){
+                    max=currsum;
+                    end=i;
+                }
+                if(currsum<0){
+                    currsum=0;
+                    start=i+1;
+                }
+            }
+            System.out.println("Start : "+start+" end : "+end);
+        }
+    
+}
